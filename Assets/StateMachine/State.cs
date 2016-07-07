8cdd07280627ea4ba9e84c2765e256553bdd68f3
CameronPenner <camdpenner@gmail.com>
2016-07-07 09:19:45 -0500

    better state inspector handling, folder restructure

---
+++ Assets/StateMachine/State.cs
@@ -0,0 +1,65 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Inherit from State to use components in the state machine
+/// </summary>
+[RequireComponent(typeof(StateMachine))]
+public class State : MonoBehaviour
+{
+	//All states have a reference to the StateMachine
+	[HideInInspector]
+	public StateMachine StateMachine;
+
+	//Use SetState from any State class to switch states
+	protected void SetState(State state)
+	{
+		StateMachine.SetState(state);
+	}
+}
+
+/// <summary>
+/// Handles State's GUI, makes sure only one is active at a time
+/// </summary>
+[CustomEditor(typeof(State), true)]
+[CanEditMultipleObjects]
+public class StateEditor : Editor
+{
+	//reference to the component
+	private State state;
+
+	private void OnEnable()
+	{
+		//get a reference to our State component
+		state = (State)target;
+		isEnabled = state.enabled;
+	}
+
+	//keeps track of current enabled status
+	private bool isEnabled = true;
+
+	public override void OnInspectorGUI()
+	{
+		//get a reference to our State component
+		state = (State)target;
+
+		//if state changed
+		if(state.enabled != isEnabled)
+		{
+			//something changed, we will now update the lastMode
+			isEnabled = state.enabled;
+
+			//if the state was enabled
+			if(state.enabled)
+			{
+				state.StateMachine.CurrentState = state;
+			}
+
+			//update all the other states, to make sure we always have exactly 1 running state
+			state.StateMachine.UpdateReferences();
+		}
+
+		//draw childrens inspectors
+		DrawDefaultInspector();
+	}
+}
---
+++ Assets/StateMachine/StateMachine.cs
@@ -0,0 +1,102 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// The StateMachine handles state switching logic
+/// States will use it to switch between each other
+/// </summary>
+public class StateMachine : MonoBehaviour
+{
+	public State[] States;
+
+	public State CurrentState;
+
+	//disables the current state, and activatese the new one
+	public void SetState(State newState)
+	{
+		//make sure the new state is valid
+		if(newState == null)
+		{
+			throw new System.Exception("Tried to set null state");
+		}
+
+		//disable the current state
+		if(CurrentState != null)
+		{
+			CurrentState.enabled = false;
+		}
+
+		//start our new state
+		newState.enabled = true;
+		CurrentState = newState;
+	}
+
+	//updates the state and statemachine references
+	public void UpdateReferences()
+	{
+		//get list of all states in our object
+		States = GetComponents<State>();
+
+		//if we have no states selected...
+		if(CurrentState == null && States.Length > 0)
+		{
+			//...select the first state
+			CurrentState = States[0];
+		}
+
+		//for every state
+		foreach(var state in States)
+		{
+			//if it's the starting state
+			if(state == CurrentState)
+			{
+				CurrentState = state;
+				state.enabled = true;
+			}
+			else //it's not that starting state
+			{
+				state.enabled = false;
+			}
+		}
+	}
+}
+
+/// <summary>
+/// The StateMachineEditor class manages the state machine inside the inspector,
+/// Making sure the states have proper references and only the starting state is enabled
+/// </summary>
+[CustomEditor(typeof(StateMachine))]
+[CanEditMultipleObjects]
+public class StateMachineEditor : Editor
+{
+	//reference to our StateMachine instance
+	private StateMachine stateMachine;
+
+	private void OnEnable()
+	{
+		//get a reference to our StateMachine component
+		stateMachine = (StateMachine)target;
+
+		stateMachine.UpdateReferences();
+	}
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUILayout.LabelField("SwitchStates", EditorStyles.boldLabel);
+
+		//make a list of buttons to use for switching states
+		foreach(var state in stateMachine.States)
+		{
+			if(GUILayout.Button(state.GetType().Name))
+			{
+				//set as our new state
+				stateMachine.SetState(state);
+
+				if(!Application.isPlaying)
+				{
+					stateMachine.UpdateReferences();
+				}
+			}
+		}
+	}
+}
